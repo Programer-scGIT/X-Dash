b53776dbbafbd67dce8882f1ae7ed2d525f1d199
Programer-scGIT <iphone240828@gmail.com>
2026-05-27 20:59:36 +0300

    fix: встроенный base path от Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,35 +2,37 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-export default defineConfig({
-    // МЕНЯЕМ СТРOЧКУ ТУТ (сделай её пустой или './'):
-    base: './',
-    plugins: [
-        react(),
-        VitePWA({
-            registerType: 'autoUpdate',
-            manifest: {
-                name: 'X-Dash Geometry Game',
-                short_name: 'X-Dash',
-                description: 'Geometry Dash Runner Game',
-                theme_color: '#0f172a',
-                background_color: '#0f172a',
-                display: 'standalone',
-                orientation: 'landscape',
-                icons: [
-                    {
-                        src: 'icon.png',
-                        sizes: '512x512',
-                        type: 'image/png'
-                    },
-                    {
-                        src: 'icon.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                        purpose: 'any maskable'
-                    }
-                ]
-            }
-        })
-    ]
+export default defineConfig(({ command }) => {
+    return {
+        // Если собираем проект (build), ставим путь репозитория, если запускаем локально — корень
+        base: command === 'build' ? '/X-Dash/' : '/',
+        plugins: [
+            react(),
+            VitePWA({
+                registerType: 'autoUpdate',
+                manifest: {
+                    name: 'X-Dash Geometry Game',
+                    short_name: 'X-Dash',
+                    description: 'Geometry Dash Runner Game',
+                    theme_color: '#0f172a',
+                    background_color: '#0f172a',
+                    display: 'standalone',
+                    orientation: 'landscape',
+                    icons: [
+                        {
+                            src: 'icon.png',
+                            sizes: '512x512',
+                            type: 'image/png'
+                        },
+                        {
+                            src: 'icon.png',
+                            sizes: '512x512',
+                            type: 'image/png',
+                            purpose: 'any maskable'
+                        }
+                    ]
+                }
+            })
+        ]
+    }
 })
